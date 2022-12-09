5cde35dfa55f80a0231a35e71d64f4e22543106b
w32u <4545.34rus@gmail.com>
2022-12-09 12:16:13 +0300

    added ability to load pictures in paint game

--- paint.py
+++ paint.py
@@ -1,105 +1,137 @@
 from tkinter import *
+from tkinter import ttk, colorchooser
+from PIL import ImageTk, Image
 
+class main:
+    def __init__(self,master):
+        self.master = master
+        self.color_fg = 'black'
+        self.color_bg = 'white'
+        self.old_x = None
+        self.old_y = None
+        self.penwidth = 5
+        self.screen_width = self.master.winfo_screenwidth()
+        self.screen_height = self.master.winfo_screenheight()
+        self.drawWidgets()
+        self.c.bind('<B1-Motion>',self.paint)#drwaing the line 
+        self.c.bind('<ButtonRelease-1>',self.reset)
 
-class Paint(Frame):
+    def paint(self,e):
+        if self.old_x and self.old_y:
+            self.c.create_line(self.old_x,self.old_y,e.x,e.y,width=self.penwidth,fill=self.color_fg,capstyle=ROUND,smooth=True)
 
-    def __init__(self, parent):
-        Frame.__init__(self, parent)
+        self.old_x = e.x
+        self.old_y = e.y
 
-        self.parent = parent
-        self.color = "black"
-        self.brush_size = 2
-        self.setUI()
+    def reset(self,e):    #reseting or cleaning the canvas 
+        self.old_x = None
+        self.old_y = None      
 
-    def set_color(self, new_color):
-        self.color = new_color
+    def changeW(self,e): #change Width of pen through slider
+        self.penwidth = e
+           
 
-    def set_brush_size(self, new_size):
-        self.brush_size = new_size
+    def clear(self):
+        self.c.delete(ALL)
 
-    def draw(self, event):
-        self.canv.create_oval(event.x - self.brush_size,
-                              event.y - self.brush_size,
-                              event.x + self.brush_size,
-                              event.y + self.brush_size,
-                              fill=self.color, outline=self.color)
+    def change_fg(self):  #changing the pen color
+        self.color_fg=colorchooser.askcolor(color=self.color_fg)[1]
 
-    def setUI(self):
+    def change_bg(self):  #changing the background color canvas
+        self.color_bg=colorchooser.askcolor(color=self.color_bg)[1]
+        self.c['bg'] = self.color_bg
+        
+    def load_image(self, imagePath):
+        self.image = Image.open(imagePath)
+        self.photo = ImageTk.PhotoImage(self.image)
+        
+        self.pw, self.ph = self.photo.width(), self.photo.height()
+        '''
+        if self.pw > self.c.winfo_width():
+            self.kw = round(self.pw // self.c.winfo_width())
+            self.pw = round(self.pw / self.kw)
+        
+        if self.ph > self.c.winfo_height():
+            self.kh = round(self.ph / self.c.winfo_height())
+            self.ph = round(self.ph / self.kh)
+        '''
+        self.ph_resized = int(self.c.winfo_width() * self.ph / self.pw)
+        self.pw_resized = int(self.c.winfo_height() * self.pw / self.ph)
+        
+        
+        self.image_resized = self.image.resize((self.pw_resized, self.ph_resized),Image.Resampling.LANCZOS)
+        self.photo_resized = ImageTk.PhotoImage(self.image_resized)
+        
+        
+        self.c.create_image(self.c.winfo_width()/2,self.c.winfo_height()/2,image=self.photo_resized)
+        
+    def load_image1(self):
+        self.load_image('res/1.jpg')
+    
+    def load_image2(self):
+        self.load_image('res/2.jpg')
+    
+    def load_image3(self):
+        self.load_image('res/3.jpg')
+    
+    def load_image4(self):
+        self.load_image('res/4.jpg')
+    
+    def load_image5(self):
+        self.load_image('res/5.jpg')
 
-        self.parent.title("Paint_ChildGame")  # Устанавливаем название окна
-        self.pack(fill=BOTH, expand=1)  # Размещаем активные элементы на родительском окне
+    def drawWidgets(self):
+        self.controls = Frame(self.master,padx = 5,pady = 5)
+        Label(self.controls, text='Размер кисти:',font=('arial 18')).grid(row=0,column=0)
+        self.slider = ttk.Scale(self.controls,from_= 5, to = 100,command=self.changeW,orient=VERTICAL)
+        self.slider.set(self.penwidth)
+        self.slider.grid(row=0,column=1,ipadx=30)
+        self.controls.pack(side=LEFT)
+        
+        self.c = Canvas(self.master,width=self.screen_width/1.5,height=self.screen_height/1.5,bg=self.color_bg,)
+        self.c.pack(fill=BOTH,expand=True)
+        
+        
 
-        self.columnconfigure(6, weight=1) # Даем седьмому столбцу возможность растягиваться, благодаря чему кнопки не будут разъезжаться при ресайзе
-        self.rowconfigure(2, weight=1) # То же самое для третьего ряда
-
-        self.canv = Canvas(self, bg="LightSteelBlue1")  # Создаем поле для рисования, устанавливаем белый фон
-        self.canv.grid(row=2, column=0, columnspan=7,
-                       padx=5, pady=5, sticky=E+W+S+N)  # Прикрепляем канвас методом grid. Он будет находится в 3м ряду, первой колонке, и будет занимать 7 колонок, задаем отступы по X и Y в 5 пикселей, и заставляем растягиваться при растягивании всего окна
-        self.canv.bind("<B1-Motion>", self.draw) # Привязываем обработчик к канвасу. <B1-Motion> означает "при движении зажатой левой кнопки мыши" вызывать функцию draw
-
-        color_lab = Label(self, text="Цвет: ") # Создаем метку для кнопок изменения цвета кисти
-        color_lab.grid(row=0, column=0, padx=6) # Устанавливаем созданную метку в первый ряд и первую колонку, задаем горизонтальный отступ в 6 пикселей
-
-        red_btn = Button(self, text="Красный", width=10,
-                         command=lambda: self.set_color("red")) # Создание кнопки:  Установка текста кнопки, задание ширины кнопки (10 символов), функция вызываемая при нажатии кнопки.
-        red_btn.grid(row=0, column=1) # Устанавливаем кнопку
-
-        # Создание остальных кнопок повторяет ту же логику, что и создание
-        # кнопки установки красного цвета, отличаются лишь аргументы.
-
-        green_btn = Button(self, text="Зеленый", width=10,
-                           command=lambda: self.set_color("green"))
-        green_btn.grid(row=0, column=2)
-
-        blue_btn = Button(self, text="Синий", width=10,
-                          command=lambda: self.set_color("blue"))
-        blue_btn.grid(row=0, column=3)
-
-        black_btn = Button(self, text="Чёрный", width=10,
-                           command=lambda: self.set_color("black"))
-        black_btn.grid(row=0, column=4)
-
-        white_btn = Button(self, text="Белый", width=10,
-                           command=lambda: self.set_color("white"))
-        white_btn.grid(row=0, column=5)
-
-        clear_btn = Button(self, text="Очистить всё", width=10,
-                           command=lambda: self.canv.delete("all"))
-        clear_btn.grid(row=0, column=6, sticky=W)
-
-        size_lab = Label(self, text="Размер кисти: ")
-        size_lab.grid(row=1, column=0, padx=5)
-        one_btn = Button(self, text="2", width=10,
-                         command=lambda: self.set_brush_size(2))
-        one_btn.grid(row=1, column=1)
-
-        two_btn = Button(self, text="5", width=10,
-                         command=lambda: self.set_brush_size(5))
-        two_btn.grid(row=1, column=2)
-
-        five_btn = Button(self, text="7", width=10,
-                          command=lambda: self.set_brush_size(7))
-        five_btn.grid(row=1, column=3)
-
-        seven_btn = Button(self, text="10", width=10,
-                           command=lambda: self.set_brush_size(10))
-        seven_btn.grid(row=1, column=4)
-
-        ten_btn = Button(self, text="20", width=10,
-                         command=lambda: self.set_brush_size(20))
-        ten_btn.grid(row=1, column=5)
-
-        twenty_btn = Button(self, text="50", width=10,
-                            command=lambda: self.set_brush_size(50))
-        twenty_btn.grid(row=1, column=6, sticky=W)
-
-
-def main():
-    root = Tk()
-    root.geometry("850x500+300+300")
-    app = Paint(root)
-    root.mainloop()
-
+        menu = Menu(self.master)
+        self.master.config(menu=menu)
+        filemenu = Menu(menu)
+        colormenu = Menu(menu)
+        menu.add_cascade(label='Картинка', menu=filemenu)
+        filemenu.add_command(label='Картинка 1',command=self.load_image1)
+        filemenu.add_command(label='Картинка 2',command=self.load_image2)
+        filemenu.add_command(label='Картинка 3',command=self.load_image3)
+        filemenu.add_command(label='Картинка 4',command=self.load_image4)
+        filemenu.add_command(label='Картинка 5',command=self.load_image5)
+        menu.add_cascade(label='Цвета',menu=colormenu)
+        colormenu.add_command(label='Цвет кисти',command=self.change_fg)
+        colormenu.add_command(label='Цвет фона',command=self.change_bg)
+        optionmenu = Menu(menu)
+        menu.add_cascade(label='Опции',menu=optionmenu)
+        optionmenu.add_command(label='Очистить лист',command=self.clear)
+        optionmenu.add_command(label='Выйти',command=self.master.destroy) 
+        
+        
 
 if __name__ == '__main__':
-    main()
+    root = Tk()
+    main(root)
+    root.title('ChildPaint')
+    root.mainloop()
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
